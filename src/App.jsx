a5eb6fd7d6b6a699dfb529e0a89c49a902843875
Danny Caceres <dannyphanth@gmail.com>
2026-03-16 15:01:47 -0700

    Took out Datathon popup

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Board from './components/Contact.jsx'
 import Projects from './components/Projects'
 import Resources from './components/Resources'
 import Contact from './components/Contact'
-import DatathonPopup from './components/DatathonPopup'
+// import DatathonPopup from './components/DatathonPopup'
 import './App.css'
 
 const theme = createTheme({
@@ -126,7 +126,7 @@ function AppShell() {
         </Routes>
       </Box>
       <Footer />
-      <DatathonPopup />
+      {/* <DatathonPopup /> */}
     </Box>
   )
 }
